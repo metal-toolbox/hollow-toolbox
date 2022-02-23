0d5a463a849288bca851153e948411784ef4010f
Juan Osorio Robles <jaosorior@gmail.com>
2022-02-23 08:02:36 +0200

    Fix typo in function (#7)
    This fixes a typo in a function name before its taken into use.
    
    Signed-off-by: Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>

--- ginjwt/multitokenmiddleware.go
+++ ginjwt/multitokenmiddleware.go
@@ -2,8 +2,8 @@ package ginjwt
 
 import "go.hollow.sh/toolbox/ginauth"
 
-// NewMultiTokenMiddlwareFromConfigs builds a MultiTokenMiddleware object from multiple AuthConfigs.
-func NewMultiTokenMiddlwareFromConfigs(cfgs ...AuthConfig) (*ginauth.MultiTokenMiddleware, error) {
+// NewMultiTokenMiddlewareFromConfigs builds a MultiTokenMiddleware object from multiple AuthConfigs.
+func NewMultiTokenMiddlewareFromConfigs(cfgs ...AuthConfig) (*ginauth.MultiTokenMiddleware, error) {
 	mtm := &ginauth.MultiTokenMiddleware{}
 
 	for _, cfg := range cfgs {
