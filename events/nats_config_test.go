29d4d9bb4a3229ed7a4f4c714f66ded9e2e6721e
Joel Rebello <mail.joelr@gmail.com>
2023-04-03 09:43:42 +0200

    events package (#90)
    * events: add EventStreamBroker and its NATS implementation
    
    This adds boilerplate code to initialize NATS streams, consumers, subscribers,
    it simplifies setting up the NATS connection based on the given configuration.
    
    The Message interface is implemented to unserialize the data as a *pubsubx.Message
    and ack/nak NATS messages.
    
    * events/nats_config: split validate methods by type
    
    * events: Add README with example
    
    * events/nats: add nil checks on the JetStream context
    
    * events/nats config: require consumer name attribute
    
    * events: rename interface StreamBroker to Stream, lint and unbork spellings
    
    * events/nats: use consumer name attribute when creating consumers for consistency

---
+++ events/nats_config_test.go
@@ -0,0 +1,194 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNatsOptions_ValidatePrereqs(t *testing.T) {
+	type fields struct {
+		URL            string
+		AppName        string
+		StreamUser     string
+		StreamPass     string
+		CredsFile      string
+		ConnectTimeout time.Duration
+	}
+
+	tests := []struct {
+		name           string
+		fields         fields
+		errorContains  string
+		wantParameters *NatsOptions
+	}{
+		{
+			"AppName required",
+			fields{},
+			"AppName not defined",
+			nil,
+		},
+		{
+			"NATS URL required",
+			fields{AppName: "foo"},
+			"server URL not defined",
+			nil,
+		},
+		{
+			"Creds file or Stream user and password credentials required",
+			fields{AppName: "foo", URL: "nats://nats:4222"},
+			"creds file",
+			nil,
+		},
+		{
+			"Stream user requires a password",
+			fields{AppName: "foo", URL: "nats://nats:4222", StreamUser: "foo"},
+			"requires a password",
+			nil,
+		},
+		{
+			"Default connect timeout is set",
+			fields{AppName: "foo", URL: "nats://nats:4222", StreamUser: "foo", StreamPass: "bar", ConnectTimeout: 200 * time.Millisecond},
+			"",
+			&NatsOptions{AppName: "foo", URL: "nats://nats:4222", StreamUser: "foo", StreamPass: "bar", ConnectTimeout: 200 * time.Millisecond},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &NatsOptions{
+				URL:            tt.fields.URL,
+				AppName:        tt.fields.AppName,
+				StreamUser:     tt.fields.StreamUser,
+				StreamPass:     tt.fields.StreamPass,
+				CredsFile:      tt.fields.CredsFile,
+				ConnectTimeout: tt.fields.ConnectTimeout,
+			}
+			err := o.validatePrereqs()
+			if tt.errorContains != "" {
+				assert.True(t, errors.Is(err, ErrNatsConfig))
+				assert.ErrorContains(t, err, tt.errorContains)
+			}
+
+			if tt.wantParameters != nil {
+				assert.Equal(t, tt.wantParameters, o)
+			}
+		})
+	}
+}
+
+func TestNatsStreamOptions_Validate(t *testing.T) {
+	type fields struct {
+		Name             string
+		Subjects         []string
+		Acknowledgements bool
+		DuplicateWindow  time.Duration
+		Retention        string
+	}
+
+	tests := []struct {
+		name           string
+		fields         fields
+		errorContains  string
+		wantParameters *NatsStreamOptions
+	}{
+		{
+			"Invalid retention parameter",
+			fields{Retention: "foobar"},
+			"require a valid Retention",
+			nil,
+		},
+		{
+			"Stream Name required",
+			fields{},
+			"require a Name",
+			nil,
+		},
+		{
+			"Subjects to associate with stream required",
+			fields{Retention: "limits", Name: "hollow"},
+			"require one or more Subjects",
+			nil,
+		},
+		{
+			"Default retention set",
+			fields{Name: "hollow", Subjects: []string{"foo.bar"}},
+			"",
+			&NatsStreamOptions{Name: "hollow", Subjects: []string{"foo.bar"}, Retention: "limits"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &NatsStreamOptions{
+				Name:             tt.fields.Name,
+				Subjects:         tt.fields.Subjects,
+				Acknowledgements: tt.fields.Acknowledgements,
+				DuplicateWindow:  tt.fields.DuplicateWindow,
+				Retention:        tt.fields.Retention,
+			}
+
+			err := s.validate()
+			if tt.errorContains != "" {
+				assert.True(t, errors.Is(err, ErrNatsConfig))
+				assert.ErrorContains(t, err, tt.errorContains)
+			}
+
+			if tt.wantParameters != nil {
+				assert.Equal(t, tt.wantParameters, s)
+			}
+		})
+	}
+}
+
+func TestNatsConsumerOptions_Validate(t *testing.T) {
+	type fields struct {
+		Pull              bool
+		Name              string
+		QueueGroup        string
+		AckWait           time.Duration
+		MaxAckPending     int
+		FilterSubject     string
+		SubscribeSubjects []string
+	}
+
+	tests := []struct {
+		name          string
+		errorContains string
+		fields        *fields
+		want          *NatsConsumerOptions
+	}{
+		{
+			"Consumer Name required",
+			"require a Name",
+			&fields{},
+			nil,
+		},
+		{
+			"Defaults set",
+			"",
+			&fields{Name: "foo"},
+			&NatsConsumerOptions{
+				Name:          "foo",
+				AckWait:       consumerAckWait,
+				MaxAckPending: consumerMaxAckPending,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &NatsConsumerOptions{Name: tt.fields.Name}
+			err := c.validate()
+			if tt.errorContains != "" {
+				assert.True(t, errors.Is(err, ErrNatsConfig))
+				assert.ErrorContains(t, err, tt.errorContains)
+			}
+
+			if tt.want != nil {
+				assert.Equal(t, tt.want, c)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,17 @@ go 1.19
 
 require (
 	github.com/gin-gonic/gin v1.9.0
+	github.com/hashicorp/go-multierror v1.1.1
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/nats-io/nats.go v1.24.0
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.2
+	go.infratographer.com/x v0.0.3
 	go.uber.org/zap v1.24.0
+	golang.org/x/exp v0.0.0-20230310171629-522b1b587ee0
 	golang.org/x/net v0.8.0
 	gopkg.in/square/go-jose.v2 v2.6.0
 )
@@ -24,8 +29,10 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -34,6 +41,9 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.15 // indirect
+	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
@@ -43,9 +53,9 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
