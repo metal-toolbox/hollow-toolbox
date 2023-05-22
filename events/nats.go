3d500223c3d582f29cc73a787f54a42a1e142d1e
Doctor Vince <DoctorVin@users.noreply.github.com>
2023-05-22 09:49:43 -0400

    add the PublisherSubjectPrefix (#117)
    * add the PublisherSubjectPrefix
    
    The NatsJetstream object can be configured with a subject-prefix to
    disambiguate subjects between participants on a NATS server. Without
    this change, the prefix will be ignored on a publish, resulting in
    rejected (or worse, misdirected) messages.
    
    * make description of publishing subject explicit
    
    A consumer of NatsJetstream may not have visibility into how the
    connection to NATS was configured (and likely isn't interested). This
    change tries to make that relationship more explicit, indicating that
    the subject provided by the caller of Publish may only represent a
    fraction of the subject to which the message is actually published.

--- events/nats.go
+++ events/nats.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"log"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/go-multierror"
@@ -223,8 +224,9 @@ func (n *NatsJetstream) addConsumer() error {
 }
 
 // Publish publishes an event onto the NATS Jetstream. The caller is responsible for message
-// addressing and data serialization.
-func (n *NatsJetstream) Publish(_ context.Context, subject string, data []byte) error {
+// addressing and data serialization. NOTE: The subject passed here will be prepended with any
+// configured PublisherSubjectPrefix.
+func (n *NatsJetstream) Publish(_ context.Context, subjectSuffix string, data []byte) error {
 	if n.jsctx == nil {
 		return errors.Wrap(ErrNatsJetstreamAddConsumer, "Jetstream context is not setup")
 	}
@@ -234,7 +236,13 @@ func (n *NatsJetstream) Publish(_ context.Context, subject string, data []byte) 
 		nats.RetryAttempts(-1),
 	}
 
-	_, err := n.jsctx.Publish(subject, data, options...)
+	fullSubject := strings.Join(
+		[]string{
+			n.parameters.PublisherSubjectPrefix,
+			subjectSuffix,
+		}, ".")
+
+	_, err := n.jsctx.Publish(fullSubject, data, options...)
 	return err
 }
 
