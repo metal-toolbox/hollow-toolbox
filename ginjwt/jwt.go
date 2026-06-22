3b90a4aae033f0327f485c0de9b15355c8a462fc
Bailin He <bahe@equinix.com>
2026-06-22 16:15:20 -0400

    fix(deps): update go-jose package import paths to v2.6.3
    Signed-off-by: Bailin He <bahe@equinix.com>

--- ginjwt/jwt.go
+++ ginjwt/jwt.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
-	"gopkg.in/square/go-jose.v2"
-	"gopkg.in/square/go-jose.v2/jwt"
+	"gopkg.in/go-jose/go-jose.v2"
+	"gopkg.in/go-jose/go-jose.v2/jwt"
 
 	"go.hollow.sh/toolbox/ginauth"
 )
